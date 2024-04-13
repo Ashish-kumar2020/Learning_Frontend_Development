b0e32d98b679251982d6417d5d09a17af116b3cd
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2024-04-13 14:55:31 +0530

    BgChanger

--- learning-react/src/App.jsx
+++ learning-react/src/App.jsx
@@ -1,11 +1,16 @@
-import Counter from "./component/Counter";
+// import Counter from "./component/Counter";
+// import Card from "./component/Card";
+import BackgroundChanger from "./component/BackgroundChanger";
 import "./index.css";
 function App() {
   return (
-    <>
-      <h1>Hi From Vite</h1>
-      <Counter />
-    </>
+    <div>
+      {/* <h1>Hi From Vite</h1> */}
+      {/* <Counter /> */}
+      {/* <Card userName="Delta Singh" des="lorem20" />
+      <Card userName="Punjab Singh" des="lorem70" /> */}
+      <BackgroundChanger />
+    </div>
   );
 }
 
