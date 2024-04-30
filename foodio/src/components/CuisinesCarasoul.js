afdd577132ff81ac871b60d7bb532df43077a47e
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2024-04-30 23:43:44 +0530

    use context api to fetch data and create carausal

--- foodio/src/components/CuisinesCarasoul.js
+++ foodio/src/components/CuisinesCarasoul.js
@@ -1,44 +1,78 @@
 import React, { useState } from "react";
+import useSwiggyApi from "../Hooks/useSwiggyApi";
 import GreaterThan from "../assets/greater_then.png";
 import LesserThan from "../assets/lesser_then.png";
-import Logo_1 from "../assets/images_1.jpeg";
-import Cart_1 from "../assets/Cart_1.png";
-import Help_1 from "../assets/Help_1.png";
-import Offer_1 from "../assets/Offer_1.png";
-import Search_1 from "../assets/Search_1.png";
-import Sigin_1 from "../assets/Sigin_1.png";
 function CuisinesCarasoul() {
-  const images = [Logo_1, Cart_1, Help_1, Offer_1, Search_1, Sigin_1];
-  const [currentImage, setCurrentImage] = useState(0);
+  const apiData = useSwiggyApi();
+  const headingCarousal = apiData?.data?.cards[0]?.card?.card?.header?.title;
+  const carousalData =
+    apiData?.data?.cards[0]?.card?.card?.imageGridCards?.info;
+  console.log(carousalData);
+  const baseUrl =
+    "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_144,h_180/";
 
-  const handlePrevImage = () => {
-    setCurrentImage((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    );
-  };
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const handleNextImage = () => {
-    setCurrentImage((prevIndex) =>
-      prevIndex === images.length - 1 ? 0 : prevIndex + 1
-    );
+    if (carousalData && currentIndex < carousalData.length) {
+      setCurrentIndex((prevIndex) => prevIndex + 1);
+    }
   };
+
+  const handlePrevImage = () => {
+    if (carousalData && currentIndex > 0) {
+      setCurrentIndex((prevIndex) => prevIndex - 1);
+    }
+  };
+
   return (
-    <div>
-      <button onClick={handlePrevImage}>
-        <img
-          className="w-[26px] mt-[-31px]"
-          src={LesserThan}
-          alt="Lesser Than"
-        />
-      </button>
-      <img
-        className="w-[26px]"
-        src={images[currentImage]}
-        alt={`Slide ${currentImage}`}
-      />
-      <button onClick={handleNextImage}>
-        <img className="w-[38px]" src={GreaterThan} alt="Greater Than" />
-      </button>
+    <div className="mt-[20px]">
+      <div className="flex justify-around">
+        <h2 className="text-2xl">{headingCarousal}</h2>
+        <div>
+          <button onClick={handlePrevImage}>
+            {" "}
+            <img
+              className="w-[27px] mt-[-32px]"
+              src={LesserThan}
+              alt="Lesser Than"
+            />
+          </button>
+          <button
+            onClick={handleNextImage}
+            disabled={!carousalData || currentIndex === carousalData.length - 1}
+          >
+            <img className="w-[38px]" src={GreaterThan} alt="Greater Than" />
+          </button>
+        </div>
+      </div>
+      <div className="carousel flex justify-center">
+        {carousalData &&
+          carousalData.map((ele, index) => {
+            const lastIndex = carousalData.length - 1;
+            const isFirstVisible =
+              index >= currentIndex && index < currentIndex + 6;
+            const isLastVisible =
+              currentIndex > lastIndex - 5 &&
+              index >= lastIndex - 5 &&
+              index <= lastIndex;
+
+            return (
+              <img
+                className={`carousel-image ${
+                  isFirstVisible || isLastVisible ? "visible" : "hidden"
+                }`}
+                key={ele.id}
+                src={`${baseUrl}${ele.imageId}`}
+                alt={ele.accessibility.altText}
+                style={{
+                  transition: "opacity 0.5s ease",
+                  opacity: isFirstVisible || isLastVisible ? 1 : 0,
+                }}
+              />
+            );
+          })}
+      </div>
     </div>
   );
 }
