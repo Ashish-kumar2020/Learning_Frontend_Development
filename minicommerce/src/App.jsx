e7351ff665e59323366397217a78ab41a9db951e
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2024-05-20 22:42:52 +0530

    fetch data from api and store that data in redux store using createAsyncThunk

--- minicommerce/src/App.jsx
+++ minicommerce/src/App.jsx
@@ -1,10 +1,21 @@
 import Header from "./components/Header";
 // import SubHeader from "./components/Header";
-
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "./redux/slice/commerceSlice";
 function App() {
+  const dispatch = useDispatch();
   return (
     <>
       <Header />
+      <div>
+        <button
+          onClick={() => {
+            dispatch(fetchProducts());
+          }}
+        >
+          Fetch Products
+        </button>
+      </div>
     </>
   );
 }
