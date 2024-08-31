33e853b36fbe3010e6f182dc4ad3169f57f60c0e
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2024-08-31 11:03:44 +0530

    added modal

--- Todo_App/index.js
+++ Todo_App/index.js
@@ -1,1 +1,20 @@
 console.log("Todo Web App");
+
+const modal = document.getElementById("myModal");
+const openModal = document.getElementById("openModalBtn");
+const closeModal = document.getElementsByClassName("close")[0];
+
+openModal.onclick = function () {
+  modal.style.display = "block";
+};
+
+closeModal.onclick = function () {
+  modal.style.display = "none";
+};
+
+// when user click outside of modal : this will close the modal
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
