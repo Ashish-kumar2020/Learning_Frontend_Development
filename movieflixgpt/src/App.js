713e772eb1d50ac28756d8100ead698c441c4d2c
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2024-09-28 13:48:04 +0530

    setup tailwindcss

--- movieflixgpt/src/App.js
+++ movieflixgpt/src/App.js
@@ -1,11 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <span>Hi from react</span>
-    </div>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
